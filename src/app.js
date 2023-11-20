b73729b17a7d32d2065c7630d3c6b8d1b3e0ec21
Carlos Salazar <carlos.salazar@funiber.org>
2023-11-20 11:13:23 -0500

    test

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ import loansRouter from './routes/loans.js';
 import { errorHandler } from './middlewares/error_handling.js';
 import { logger } from './logging/logger.js';
 import morgan from 'morgan';
+import fs from 'fs';
 
 const app = express();
 
@@ -28,5 +29,24 @@ app.use(booksRouter);
 app.use(authorsRouter);
 app.use(usersRouter);
 app.use(loansRouter);
+
+app. get('/test', (req, res) => {
+  //res.send('Hello World!');  
+  res.json(readJSONFile('/app/src/payments.json'));
+});
+
+function readJSONFile(filename) {
+  try {
+    // Note that jsonString will be a <Buffer> since we did not specify an
+    // encoding type for the file. But it'll still work because JSON.parse() will
+    // use <Buffer>.toString().
+    const jsonString = fs.readFileSync(filename);
+    const payments = JSON.parse(jsonString);
+    return payments;
+  } catch (err) {
+    return {error: err};
+  }
+  
+}
 
 export default app;
